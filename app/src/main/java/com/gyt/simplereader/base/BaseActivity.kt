9a519ed6490c7b29e140e0003bae4409e0c927da
guoyiteng <teng13114@qq.com>
2019-01-18 17:44:18 +0800

    init

---
+++ app/src/main/java/com/gyt/simplereader/base/BaseActivity.kt
@@ -0,0 +1,20 @@
+package com.gyt.simplereader.base
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * @author gyt
+ * @date on 2019/1/18 4:40 PM
+ * @describer TODO
+ */
+
+abstract class BaseActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+
+}
